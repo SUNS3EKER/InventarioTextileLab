b0bca8766247b87dfdea2a697444ee4aa211eefa
Sunseeker <112045261+SUNS3EKER@users.noreply.github.com>
2025-03-15 15:46:05 -0600

    Add files via upload

---
+++ INVENTARIO/INVENTARIO/Form1.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        DataGridView1 = New DataGridView()
+        TBregistro = New TextBox()
+        BTcrear = New Button()
+        BTborrar = New Button()
+        BTbuscar = New Button()
+        BTactualizar = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        CBregistro = New CheckBox()
+        CBXmodelo = New ComboBox()
+        CBXcolor = New ComboBox()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.AllowUserToAddRows = False
+        DataGridView1.AllowUserToDeleteRows = False
+        DataGridView1.AllowUserToResizeColumns = False
+        DataGridView1.AllowUserToResizeRows = False
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(30, 32)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.ReadOnly = True
+        DataGridView1.Size = New Size(357, 387)
+        DataGridView1.StandardTab = True
+        DataGridView1.TabIndex = 0
+        DataGridView1.TabStop = False
+        ' 
+        ' TBregistro
+        ' 
+        TBregistro.Location = New Point(408, 65)
+        TBregistro.Name = "TBregistro"
+        TBregistro.Size = New Size(119, 23)
+        TBregistro.TabIndex = 1
+        ' 
+        ' BTcrear
+        ' 
+        BTcrear.AutoSize = True
+        BTcrear.Location = New Point(420, 220)
+        BTcrear.Name = "BTcrear"
+        BTcrear.Size = New Size(124, 86)
+        BTcrear.TabIndex = 4
+        BTcrear.Text = "Insertar"
+        BTcrear.UseVisualStyleBackColor = True
+        ' 
+        ' BTborrar
+        ' 
+        BTborrar.AutoSize = True
+        BTborrar.Location = New Point(420, 312)
+        BTborrar.Name = "BTborrar"
+        BTborrar.Size = New Size(124, 86)
+        BTborrar.TabIndex = 5
+        BTborrar.Text = "Borrar"
+        BTborrar.UseVisualStyleBackColor = True
+        ' 
+        ' BTbuscar
+        ' 
+        BTbuscar.AutoSize = True
+        BTbuscar.Location = New Point(550, 312)
+        BTbuscar.Name = "BTbuscar"
+        BTbuscar.Size = New Size(124, 86)
+        BTbuscar.TabIndex = 6
+        BTbuscar.Text = "Buscar"
+        BTbuscar.UseVisualStyleBackColor = True
+        ' 
+        ' BTactualizar
+        ' 
+        BTactualizar.AutoSize = True
+        BTactualizar.Location = New Point(550, 220)
+        BTactualizar.Name = "BTactualizar"
+        BTactualizar.Size = New Size(124, 86)
+        BTactualizar.TabIndex = 7
+        BTactualizar.Text = "Actualizar"
+        BTactualizar.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(442, 47)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(50, 15)
+        Label1.TabIndex = 8
+        Label1.Text = "Registro"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(442, 132)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(48, 15)
+        Label2.TabIndex = 9
+        Label2.Text = "Modelo"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(442, 89)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(36, 15)
+        Label3.TabIndex = 10
+        Label3.Text = "Color"
+        ' 
+        ' CBregistro
+        ' 
+        CBregistro.AutoSize = True
+        CBregistro.Checked = True
+        CBregistro.CheckState = CheckState.Checked
+        CBregistro.Location = New Point(533, 65)
+        CBregistro.Name = "CBregistro"
+        CBregistro.Size = New Size(94, 19)
+        CBregistro.TabIndex = 13
+        CBregistro.TabStop = False
+        CBregistro.Text = "Auto Conteo"
+        CBregistro.UseVisualStyleBackColor = True
+        ' 
+        ' CBXmodelo
+        ' 
+        CBXmodelo.FormattingEnabled = True
+        CBXmodelo.Items.AddRange(New Object() {"P. Petalo", "P. Menta", "P. Negro", "P. Uva", "P. Olivo", "P. Chocolate", "P. Tan", "P. Lila", "P. Fucsia", "P. Lavanda", "P. Celeste", "P. Granate", "P. Rojo", "P. Blush Perlizado", "P. Inox Perlizado", "P. Blue Perlizado", "P. Rosa Perlizado", "P. Perla", "P. Oro Rosado", "P. Met. Plata", "P. Met. Lingote", "P. Met. Fucsia", "P. Met. Turquesa", "P. Met. Rojo", "P. Met. Cuarzo"})
+        CBXmodelo.Location = New Point(408, 107)
+        CBXmodelo.Name = "CBXmodelo"
+        CBXmodelo.Size = New Size(121, 23)
+        CBXmodelo.TabIndex = 14
+        ' 
+        ' CBXcolor
+        ' 
+        CBXcolor.FormattingEnabled = True
+        CBXcolor.Items.AddRange(New Object() {"Monedero", "Slim", "Petit", "Petit Plus", "Clasic", "Clasic Plus", "Neceser"})
+        CBXcolor.Location = New Point(408, 150)
+        CBXcolor.Name = "CBXcolor"
+        CBXcolor.Size = New Size(121, 23)
+        CBXcolor.TabIndex = 15
+        ' 
+        ' Form1
+        ' 
+        AutoScaleMode = AutoScaleMode.Inherit
+        AutoSize = True
+        ClientSize = New Size(704, 450)
+        Controls.Add(CBXcolor)
+        Controls.Add(CBXmodelo)
+        Controls.Add(CBregistro)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(BTactualizar)
+        Controls.Add(BTbuscar)
+        Controls.Add(BTborrar)
+        Controls.Add(BTcrear)
+        Controls.Add(TBregistro)
+        Controls.Add(DataGridView1)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        Name = "Form1"
+        Text = "Inventario"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents TBregistro As TextBox
+    Friend WithEvents BTcrear As Button
+    Friend WithEvents BTborrar As Button
+    Friend WithEvents BTbuscar As Button
+    Friend WithEvents BTactualizar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents CBregistro As CheckBox
+    Friend WithEvents CBXmodelo As ComboBox
+    Friend WithEvents CBXcolor As ComboBox
+
+End Class
---
+++ INVENTARIO/INVENTARIO/Form1.vb
@@ -0,0 +1,188 @@
+﻿Imports System.Data.OleDb
+
+Public Class Form1
+    Private connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\kalel\Desktop\INVENTARIO\InventarioTextilelab.accdb"
+    Private dataAdapter As OleDbDataAdapter
+    Private dataSet As DataSet
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadData()
+    End Sub
+
+
+    Private Sub autoconteo()
+        If CBregistro.Checked = True Then
+            Try
+                Using connection As New OleDbConnection(connectionString)
+                    connection.Open()
+                    Using command As New OleDbCommand("SELECT MAX(Registro) FROM Inventario", connection)
+                        Dim result = command.ExecuteScalar()
+                        If result IsNot DBNull.Value AndAlso result IsNot Nothing Then
+                            TBregistro.Text = (Convert.ToInt32(result) + 1).ToString()
+                        Else
+                            TBregistro.Text = "1"
+                        End If
+                    End Using
+                End Using
+            Catch ex As Exception
+                MessageBox.Show("Error al obtener el siguiente ID: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+    End Sub
+
+    Private Sub AutoConteoInvertido()
+        If CBregistro.Checked Then
+            Try
+                Using connection As New OleDbConnection(connectionString)
+                    connection.Open()
+                    Using command As New OleDbCommand("SELECT MAX(Registro) FROM Inventario WHERE Registro < ?", connection)
+                        command.Parameters.AddWithValue("?", TBregistro.Text)
+                        Dim result = command.ExecuteScalar()
+                        If result IsNot DBNull.Value AndAlso result IsNot Nothing Then
+                            TBregistro.Text = result.ToString()
+                        Else
+                            TBregistro.Text = ""
+                        End If
+                    End Using
+                End Using
+            Catch ex As Exception
+                MessageBox.Show("Error al obtener el ID anterior: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+    End Sub
+
+
+
+    Private Sub LoadData()
+        Try
+            Using connection As New OleDbConnection(connectionString)
+                dataAdapter = New OleDbDataAdapter("SELECT * FROM Inventario", connection)
+                dataSet = New DataSet()
+                dataAdapter.Fill(dataSet, "Inventario")
+                DataGridView1.DataSource = dataSet.Tables("Inventario")
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Error al cargar los datos: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        If e.RowIndex >= 0 Then
+            Dim selectedRow As DataGridViewRow = DataGridView1.Rows(e.RowIndex)
+            TBregistro.Text = selectedRow.Cells("Registro").Value.ToString()
+            CBXmodelo.Text = selectedRow.Cells("Modelo").Value.ToString()
+            CBXcolor.Text = selectedRow.Cells("Color").Value.ToString()
+        End If
+    End Sub
+
+    Private Sub BTcrear_Click(sender As Object, e As EventArgs) Handles BTcrear.Click
+        Try
+            If CBregistro.Checked = True Then
+                autoconteo()
+            End If
+
+            If String.IsNullOrWhiteSpace(TBregistro.Text) OrElse
+               String.IsNullOrWhiteSpace(CBXmodelo.Text) OrElse
+               String.IsNullOrWhiteSpace(CBXcolor.Text) Then
+                MessageBox.Show("Todos los campos son obligatorios.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Exit Sub
+            End If
+
+            Using connection As New OleDbConnection(connectionString)
+                connection.Open()
+                Using command As New OleDbCommand("INSERT INTO Inventario (Registro, Modelo, Color) VALUES (?, ?, ?)", connection)
+                    command.Parameters.AddWithValue("?", TBregistro.Text)
+                    command.Parameters.AddWithValue("?", CBXmodelo.Text)
+                    command.Parameters.AddWithValue("?", CBXcolor.Text)
+                    command.ExecuteNonQuery()
+                End Using
+            End Using
+
+            LoadData()
+            autoconteo()
+
+
+        Catch ex As Exception
+            MessageBox.Show("Error al insertar datos: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+
+    Private Sub BTactualizar_Click(sender As Object, e As EventArgs) Handles BTactualizar.Click
+        Try
+            If String.IsNullOrWhiteSpace(TBregistro.Text) Then
+                MessageBox.Show("Debes seleccionar un registro para actualizar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Exit Sub
+            End If
+
+            Using connection As New OleDbConnection(connectionString)
+                connection.Open()
+                Using command As New OleDbCommand("UPDATE Inventario SET Modelo = ?, Color = ? WHERE Registro = ?", connection)
+                    command.Parameters.AddWithValue("?", CBXmodelo.Text)
+                    command.Parameters.AddWithValue("?", CBXcolor.Text)
+                    command.Parameters.AddWithValue("?", TBregistro.Text)
+                    command.ExecuteNonQuery()
+                End Using
+            End Using
+
+            LoadData()
+
+        Catch ex As Exception
+            MessageBox.Show("Error al actualizar datos: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub BTborrar_Click(sender As Object, e As EventArgs) Handles BTborrar.Click
+        Try
+            If String.IsNullOrWhiteSpace(TBregistro.Text) Then
+                MessageBox.Show("Debe seleccionar un registro para eliminar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Exit Sub
+            End If
+
+            If MessageBox.Show("¿Está seguro de que desea eliminar este registro?", "Confirmar", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) = DialogResult.Yes Then
+                Using connection As New OleDbConnection(connectionString)
+                    connection.Open()
+                    Using command As New OleDbCommand("DELETE FROM Inventario WHERE Registro = ?", connection)
+                        command.Parameters.AddWithValue("?", TBregistro.Text)
+                        command.ExecuteNonQuery()
+                    End Using
+                End Using
+
+                LoadData()
+                AutoConteoInvertido()
+
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Error al eliminar datos: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub BTbuscar_Click(sender As Object, e As EventArgs) Handles BTbuscar.Click
+        Try
+            If String.IsNullOrWhiteSpace(TBregistro.Text) Then
+                MessageBox.Show("Debe ingresar un ID para buscar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Exit Sub
+            End If
+
+            Using connection As New OleDbConnection(connectionString)
+                Dim query As String = "SELECT * FROM Inventario WHERE Registro = ?"
+                Using command As New OleDbCommand(query, connection)
+                    command.Parameters.AddWithValue("?", TBregistro.Text)
+                    connection.Open()
+                    Using reader As OleDbDataReader = command.ExecuteReader()
+                        If reader.Read() Then
+                            CBXmodelo.Text = reader("Modelo").ToString()
+                            CBXcolor.Text = reader("Color").ToString()
+                        Else
+                            MessageBox.Show("No se encontró ningún registro con ese ID.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                        End If
+                    End Using
+                End Using
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Error al buscar datos: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+End Class
